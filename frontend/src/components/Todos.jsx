9ebd10f93e0b1c85798ab93db2094bf66c5f2c72
Tushar <t@gmail.com>
2024-07-31 14:32:26 +0530

    Add Completed function

--- frontend/src/components/Todos.jsx
+++ frontend/src/components/Todos.jsx
@@ -6,7 +6,20 @@ export function Todos({todo}){
         <div>
           <h1>{t.title}</h1>
           <h2>{t.description}</h2>
-          <button>{t.completed==false?"Mark as done":"Completed"}</button>
+          <button onClick={()=>{
+            fetch("http://localhost:3000/completed",{
+              method:"PUT",
+              body:JSON.stringify({
+                id:t._id
+              }),
+              headers:{
+                "content-type":"application/json"
+              }
+            }).then(async(data)=>
+              {const d = await data.json();
+                alert("updated");
+            })
+          }}>{t.completed==false?"Mark as done":"Completed"}</button>
         </div>
       )
     })}
